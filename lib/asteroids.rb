86631eca9a5c9fa6d66913c1f83cb2bb6d4b0b36
Svetoslav Kuzmanov <svetoslav.kuzmanov@gmail.com>
2015-02-05 14:48:08 +0200

    Create the executable for the game and basic window

--- lib/asteroids.rb
+++ lib/asteroids.rb
@@ -1,5 +1,21 @@
-require "asteroids/version"
-
 module Asteroids
-  # Your code goes here...
+  class GameWindow < Gosu::Window
+
+   def initialize
+     super(800, 600, false)
+   end
+
+   def update
+   end
+
+   def draw
+   end
+
+   def needs_redraw?
+   end
+
+   def button_down(id)
+   end
+
+  end
 end
