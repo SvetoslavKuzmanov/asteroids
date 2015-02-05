78447eb62d2e54831b2d6055a1d8ecaf317fb214
Svetoslav Kuzmanov <svetoslav.kuzmanov@gmail.com>
2015-02-05 14:56:35 +0200

    Implement the base class for game states.
    Make window object functionality to execute the state
    functionality.

--- lib/asteroids.rb
+++ lib/asteroids.rb
@@ -1,20 +1,26 @@
 module Asteroids
   class GameWindow < Gosu::Window
 
+   attr_accessor :state
+
    def initialize
      super(800, 600, false)
    end
 
    def update
+     @state.update
    end
 
    def draw
+     @state.draw
    end
 
    def needs_redraw?
+     @state.needs_redraw?
    end
 
    def button_down(id)
+     @state.button_down(id)
    end
 
   end
