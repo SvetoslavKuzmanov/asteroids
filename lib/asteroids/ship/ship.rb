4cd93860b7ca3f7ab36bb4c8416a615991fc80bd
Svetoslav Kuzmanov <svetoslav.kuzmanov@gmail.com>
2015-02-06 20:40:46 +0200

    Implement the ship shooting methods.
    The shooting functionality is implemented, so there
    is delay between each missile is shot.

--- lib/asteroids/ship/ship.rb
+++ lib/asteroids/ship/ship.rb
@@ -1,6 +1,8 @@
 module Asteroids
   class Ship < GameObject
 
+    SHOOT_DELAY = 600
+
     attr_accessor :x, :y, :vel_x, :vel_y, :angle, :thrust
 
     def initialize(object_pool)
@@ -8,6 +10,17 @@ module Asteroids
       @physics = ShipPhysics.new(self, object_pool)
       @graphics = ShipGraphics.new(self)
       @vel_x = @vel_y = @angle = 0.0
+    end
+
+    def shoot
+      if can_shoot?
+        @last_shot = Gosu.milliseconds
+        Missile.new(object_pool, @x, @y, @vel_x, @vel_y, @angle)
+      end
+    end
+
+     def can_shoot?
+      Gosu.milliseconds - (@last_shot || 0) > SHOOT_DELAY
     end
 
   end
