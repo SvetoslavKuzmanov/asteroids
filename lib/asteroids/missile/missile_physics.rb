4af0d7cbe4ccd9ea2585d8fbbd1608aaf674dc1f
Svetoslav Kuzmanov <svetoslav.kuzmanov@gmail.com>
2015-02-06 20:37:52 +0200

    Implement the update method for ship physics.

--- lib/asteroids/missile/missile_physics.rb
+++ lib/asteroids/missile/missile_physics.rb
@@ -6,5 +6,13 @@ module Asteroids
        @object_pool = object_pool
     end
 
+    def update
+      object.x += Gosu::offset_x(object.angle, 10) + object.vel_x
+      object.y += Gosu::offset_y(object.angle, 10) + object.vel_y
+      object.x %= 800
+      object.y %= 600
+      object.lifespan -= 0.1
+    end
+
   end
 end
