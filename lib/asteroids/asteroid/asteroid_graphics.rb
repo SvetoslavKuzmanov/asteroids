dfed1410da84f8d797e11d1a4914c67fac822517
Svetoslav Kuzmanov <svetoslav.kuzmanov@gmail.com>
2015-02-06 23:56:21 +0200

    Implement asteroid graphics and add asteroid image.

--- lib/asteroids/asteroid/asteroid_graphics.rb
+++ lib/asteroids/asteroid/asteroid_graphics.rb
@@ -3,6 +3,12 @@ module Asteroids
 
     def initialize(game_object)
        super(game_object)
+        @image = Gosu::Image.new($window,
+         Utils.get_image_path("asteroid.png"), true)
+    end
+
+    def draw
+      @image.draw_rot(object.x, object.y, 1, object.angle)
     end
 
   end
