b3cd4481880f53b81c3da98917e6f13ecac16037
Svetoslav Kuzmanov <svetoslav.kuzmanov@gmail.com>
2015-02-07 15:27:49 +0200

    Implement the drawing of different size asteroids.

--- lib/asteroids/asteroid/asteroid.rb
+++ lib/asteroids/asteroid/asteroid.rb
@@ -3,7 +3,7 @@ module Asteroids
 
     attr_accessor :x, :y, :vel_x, :vel_y, :angle, :radius
 
-    def initialize(object_pool, x, y, vel_x, vel_y, angle)
+    def initialize(object_pool, x, y, vel_x, vel_y, angle, radius = 45)
       super(object_pool)
       @x = x
       @y = y
@@ -12,7 +12,7 @@ module Asteroids
       @vel_y = vel_y
       @physics = AsteroidPhysics.new(self, object_pool)
       @graphics = AsteroidGraphics.new(self)
-      @radius = 40
+      @radius = radius
     end
 
   end
--- lib/asteroids/asteroid/asteroid_graphics.rb
+++ lib/asteroids/asteroid/asteroid_graphics.rb
@@ -5,10 +5,21 @@ module Asteroids
        super(game_object)
         @image = Gosu::Image.new($window,
          Utils.get_image_path("asteroid.png"), true)
+        @image_medium = Gosu::Image.new($window,
+         Utils.get_image_path("asteroid_medium.png"), true)
+        @image_small = Gosu::Image.new($window,
+         Utils.get_image_path("asteroid_small.png"), true)
     end
 
     def draw
-      @image.draw_rot(object.x, object.y, 1, object.angle)
+      case object.radius
+      when 45
+        @image.draw_rot(object.x, object.y, 1, object.angle)
+      when 30
+          @image_medium.draw_rot(object.x, object.y, 1, object.angle)
+      when 17.5
+          @image_small.draw_rot(object.x, object.y, 1, object.angle)
+      end
     end
 
   end
