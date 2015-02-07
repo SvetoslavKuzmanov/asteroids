d11cf8c6644f3531392bafa7bce02299f0e8c9a3
Svetoslav Kuzmanov <svetoslav.kuzmanov@gmail.com>
2015-02-07 12:35:27 +0200

    Implement the collision detection for the asteroid.

--- lib/asteroids/asteroid/asteroid_physics.rb
+++ lib/asteroids/asteroid/asteroid_physics.rb
@@ -7,10 +7,22 @@ module Asteroids
     end
 
     def update
+      object.mark_for_removal if detect_collision?
       object.x += Gosu::offset_x(object.angle, 0.0001) + object.vel_x
       object.y += Gosu::offset_y(object.angle, 0.0001) + object.vel_y
       object.x %= 800
       object.y %= 600
+    end
+
+    def detect_collision?
+      @object_pool.objects.each do |other_object|
+        if other_object.is_a? Asteroids::Ship or
+         other_object.is_a?  Asteroids::Missile and
+          Utils.collide(object, other_object)
+            return true
+        end
+      end
+      return false
     end
 
   end
