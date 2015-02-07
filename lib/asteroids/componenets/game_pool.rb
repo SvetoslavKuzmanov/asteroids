0b1e60ccdbba3f908f5ab1caa6be0fc5a6869052
Svetoslav Kuzmanov <svetoslav.kuzmanov@gmail.com>
2015-02-07 22:13:45 +0200

    Implement method to find empty spot for respawn.

--- lib/asteroids/componenets/game_pool.rb
+++ lib/asteroids/componenets/game_pool.rb
@@ -11,5 +11,24 @@ module Asteroids
       @objects.delete_if { |object| object.removable?}
     end
 
+    def find_empty_space
+      x = Gosu::random(0, 800)
+      y = Gosu::random(0, 600)
+      while is_not_empty(x, y)
+        x = Gosu::random(0, 800)
+        y = Gosu::random(0, 600)
+      end
+      {x: x, y: y}
+    end
+
+    def is_not_empty(x, y)
+      @objects.each do |object|
+        if object.is_a? Asteroids::Asteroid and object.x == x and object.y == y
+          return true
+        end
+      end
+      false
+    end
+
   end
 end
